550b229bd91440523940206b3c4b112c7106b370
Your Name <you@example.com>
2021-06-15 18:20:54 +0600

    hello programmer!

--- program-46.c
+++ program-46.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-    if(10 >= 15 - 5 && !0 && 5){ // (5 > 10 - 5 && 5) This rerun = False!
+    if(10 >= 15 - 5 && !0 && 5){ // (5 > 10 - 5 && 5) This return = False!
         printf("Hello Programmer!");
     }
     else{
