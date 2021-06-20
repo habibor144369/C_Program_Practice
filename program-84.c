0c69b9c45a2dc51bfc431357fd6030a04a1a71d0
Your Name <you@example.com>
2021-06-20 19:27:06 +0600

    hello Lord

--- program-84.c
+++ program-84.c
@@ -1,12 +1,13 @@
-// -100 to -1 print in C program....
+// -1 to -100 print in C program....
 #include <stdio.h>
 int main(){
     int x;
-    x = -100;
+    x = -1;
 
-    while(x <= -1){
+    while(x >= -100){
         printf("value of negative number: %d \n", x);
-        x++;
+        x--;
     }
     return 0;
 }
+
