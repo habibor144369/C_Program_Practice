3577327be57ac222fca4db61436a1a8ae531c686
Your Name <you@example.com>
2021-06-23 13:11:45 +0600

    ok done

--- program-114.c
+++ program-114.c
@@ -1,6 +1,8 @@
 // -100 to -1 odd number find out in c program using continue....
 #include <stdio.h>
+#include <stdlib.h>
 int main(){
+    system("COLOR F0");
     int i;
     for(i = -100; i <= -1; i+=1){
         if(i % 2 == 0)
